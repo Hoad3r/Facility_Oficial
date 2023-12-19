6ea8dc5f01d73ee4c28fb74a5610e82d32456d09
Pablo Marques <detup.4@gmail.com>
2023-12-19 16:46:29 -0300

    Biometria

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:facility/screens/reset_password.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +11,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  @override
+  initState() {
+    super.initState();
+    authenticate();
+  }
+
+  authenticate() async {
+    print("pré autenticação");
+    if (await authenticateIsAvailable()) {
+      print("Disponivel para autenticar");
+      await _getListOfBiometricTypes();
+      await _authenticateUser();
+    }
+  }
+
+  Future<bool> authenticateIsAvailable() async {
+    final isAvailable = await _localAuthentication.canCheckBiometrics;
+    final isDeviceSupported = await _localAuthentication.isDeviceSupported();
+    return isAvailable && isDeviceSupported;
+  }
+
+  Future<void> _getListOfBiometricTypes() async {
+    List<BiometricType> listOfBiometrics =
+        await _localAuthentication.getAvailableBiometrics();
+  }
+
+  Future<void> _authenticateUser() async {
+    bool isAuthenticated = await _localAuthentication.authenticate(
+        localizedReason: "Use a biometria para prosseguir");
+    if (isAuthenticated) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => ResetPassword(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
